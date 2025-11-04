dc673d199ce95aeb42a8ccc8290720d35091ca39
Sureka400 <surekar.aids2024@citchennai.net>
2025-11-04 10:44:38 +0530

    Add files via upload

---
+++ users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  }
+  // you can add name, roles, etc.
+});
+
+module.exports = mongoose.model('User', userSchema);
